f7f29250ce98b9bb0617d6986acad6fe75495842
339513541 <339513541@2260I-L29-016>
2023-09-13 10:05:40 -0400

    Add project files.

---
+++ Part 2 intro to numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2_intro_to_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 19;
+            int b = 10;
+            Console.WriteLine((double)a / b);
+            Console.ReadLine();
+        }
+    }
+}
